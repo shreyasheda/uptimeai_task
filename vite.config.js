844b7c1e6ba9ff7a11dc5bf8cae83722dd67c497
shreyasheda <shreyasheda1992@gmail.com>
2025-11-13 17:31:16 +0530

    added deployment on github pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
+    react(),
   ],
+  base: "https://shreyasheda.github.io/uptimeai_task/"
 })
